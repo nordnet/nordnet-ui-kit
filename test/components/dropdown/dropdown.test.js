ecb2b53a6d83f6525da940bebfe1d23654e16d2a
Gustaf Zetterlund <gustaf.zetterlund@gmail.com>
2016-02-02 18:11:37 +0100

    fixed lint error

--- test/components/dropdown/dropdown.test.js
+++ test/components/dropdown/dropdown.test.js
@@ -6,12 +6,9 @@ import Dropdown from '../../../src/components/dropdown/dropdown';
 describe('<Dropdown />', () => {
   let wrapper;
 
-  beforeEach(() => wrapper = shallow(<Dropdown toggle="Dropdown" actions={[
-    {
-      label: 'log',
-      action: function() { console.log('I was clicked!'); },
-    },
-  ]} />));
+  beforeEach(() => wrapper = shallow(
+    <Dropdown toggle="Dropdown" actions={[{ label: 'log', action() {console.log('I was clicked!');} }]} />
+  ));
 
   it('should render <div> as container', () => expect(wrapper.type()).to.equal('div'));
   it('should contain a <button> with class \'dropdown__toggle\'', () => assert.ok(wrapper.find('button').hasClass('dropdown__toggle')));
