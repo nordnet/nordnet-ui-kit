b12151cc7baa71cabfcce91edf64e388aef2ee1e
Gustaf Zetterlund <gustaf.zetterlund@gmail.com>
2016-02-29 15:20:54 +0100

    swapped to raw loader instead of inline-loader for svg's

--- examples/documentation/src/components/section/section.jsx
+++ examples/documentation/src/components/section/section.jsx
@@ -3,7 +3,6 @@ import PureComponent from 'react-pure-render/component';
 import classNames from 'classnames';
 import kebabCase from 'lodash.kebabcase';
 import { PrismCode } from 'react-prism';
-import InlineSvg from 'svg-inline-react';
 import anchorIcon from '../../assets/icons/anchor-icon.svg';
 import './section.scss';
 
@@ -37,9 +36,11 @@ class Section extends PureComponent {
         <div className="section__introduction">
           <h1 className="section__title">
             { this.props.title }
-            <a className="section__link" href={ `#${ id }-section` }>
-              <InlineSvg src={ anchorIcon } />
-            </a>
+            <a
+              className="section__link"
+              href={ `#${ id }-section` }
+              dangerouslySetInnerHTML={{ __html: anchorIcon }}
+            />
           </h1>
           <p className="section__description">
             { this.props.description }
