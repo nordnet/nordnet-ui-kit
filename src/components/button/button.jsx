d812a11b3b6e4f3af0ba2687974ec24ee19f3326
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-01 13:15:35 +0200

    implement button variant feature, fix #26

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -3,17 +3,19 @@ import classNames from 'classnames';
 import './button.scss';
 
 function Button(props) {
-  const { primary, secondary, link, modifier, href } = props;
+  const { primary, secondary, variant, link, modifier, href } = props;
   const Element = href ? 'a' : 'button';
+  const isPrimary = primary || variant === 'primary';
+  const isSecondary = secondary || variant === 'secondary';
   const classes = classNames({
-    'btn-primary': primary,
-    'btn-primary--success': primary && modifier === 'success',
-    'btn-primary--warning': primary && modifier === 'warning',
-    'btn-primary--danger': primary && modifier === 'danger',
-    'btn-secondary': secondary,
-    'btn-secondary--success': secondary && modifier === 'success',
-    'btn-secondary--warning': secondary && modifier === 'warning',
-    'btn-secondary--danger': secondary && modifier === 'danger',
+    'btn-primary': isPrimary,
+    'btn-primary--success': isPrimary && modifier === 'success',
+    'btn-primary--warning': isPrimary && modifier === 'warning',
+    'btn-primary--danger': isPrimary && modifier === 'danger',
+    'btn-secondary': isSecondary,
+    'btn-secondary--success': isSecondary && modifier === 'success',
+    'btn-secondary--warning': isSecondary && modifier === 'warning',
+    'btn-secondary--danger': isSecondary && modifier === 'danger',
     'btn-link': link,
     'btn-link--success': link && modifier === 'success',
     'btn-link--warning': link && modifier === 'warning',
@@ -30,6 +32,7 @@ function Button(props) {
 Button.propTypes = {
   children: React.PropTypes.node,
   className: React.PropTypes.string,
+  variant: React.PropTypes.string,
   primary: React.PropTypes.bool,
   secondary: React.PropTypes.bool,
   link: React.PropTypes.bool,
