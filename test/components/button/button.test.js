bd5d1298c16c378092c311ed366bfac3bd6d3de5
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-01 13:07:30 +0200

    cover button with a bit more tests

--- test/components/button/button.test.js
+++ test/components/button/button.test.js
@@ -3,10 +3,19 @@ import { shallow } from 'enzyme';
 import { expect } from 'chai';
 import Button from '../../../src/components/button/button';
 
-describe('<Button />', () => {
-  let wrapper;
+describe.only('<Button />', () => {
+  it('should render <button> by default', () => {
+    const component = shallow(<Button>Hello</Button>);
+    expect(component.type()).to.equal('button');
+  });
 
-  beforeEach(() => wrapper = shallow(<Button>Hello</Button>));
+  it('should use "primary" prop', () => {
+    const component = shallow(<Button primary>Hello</Button>);
+    expect(component.hasClass('btn-primary')).to.equal(true);
+  });
 
-  it('should render <button> by default', () => expect(wrapper.type()).to.equal('button'));
+  it('should use "secondary" prop', () => {
+    const component = shallow(<Button secondary>Hello</Button>);
+    expect(component.hasClass('btn-secondary')).to.equal(true);
+  });
 });
