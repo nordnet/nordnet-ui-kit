530915dcc13b494e9e3a12c6671ef2426d62126c
Dmitry Demyankov <dmitry.demyankov@gmail.com>
2016-04-07 23:47:15 +0200

    updated function that generates rem values

--- src/utilities/rem.js
+++ src/utilities/rem.js
@@ -2,27 +2,28 @@ import isNumber from 'lodash.isnumber';
 
 function rem(value) {
   if (__USE_REM__) {
-    const baselinePx = 10;
-    const matchNumber = /(\d*\.?\d+)\s*px/g;
-
-    if (isNumber(value)) {
-      return value / baselinePx;
-    }
-
-    if (matchNumber.test(value)) {
-      const matches = value.match(matchNumber);
-      let result = value;
-
-      matches.forEach(match => {
-        const inRem = parseFloat(match, 10) / baselinePx;
-        result = result.replace(match, `${ inRem }rem`);
-      });
-
-      return result;
-    }
+    return convertToRem(value);
   }
 
   return value;
+}
+
+function convertToRem(value) {
+  const baselinePx = 10;
+  const matchNumber = /(\d*\.?\d+)\s*px/g;
+
+  if (isNumber(value)) {
+    return value / baselinePx;
+  }
+
+  if (matchNumber.test(value)) {
+    const matches = value.match(matchNumber);
+
+    return matches.reduce((result, match) => {
+      const inRem = parseFloat(match, 10) / baselinePx;
+      return result.replace(match, `${ inRem }rem`);
+    }, value);
+  }
 }
 
 export default rem;
