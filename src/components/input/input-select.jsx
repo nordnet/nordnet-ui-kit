c684c72804aea5ecf9a45be66990d2bea2aed526
Gustaf Zetterlund <gustaf.zetterlund@gmail.com>
2016-04-22 15:37:14 +0200

    removed unused import

--- src/components/input/input-select.jsx
+++ src/components/input/input-select.jsx
@@ -1,22 +1,10 @@
 import React, { PropTypes } from 'react';
-import rem from '../../utilities/rem.js';
 import InputDefault from './input-default';
 import variables from '../../utilities/variables';
 import Icon from '../icon/icon';
 import './input-select.scss';
 
 class InputSelect extends InputDefault {
-  // constructor(props) {
-  //   super(props);
-  //
-  //   this.state = Object.assign({}, this.state, {
-  //     showPassword: props.showPassword ? true : false,
-  //     hasAddon: true,
-  //   });
-  //
-  //   this.renderPasswordToggle = this.renderPasswordToggle.bind(this);
-  //   this.onPasswordToggle = this.onPasswordToggle.bind(this);
-  // }
 
   renderSelectArrow() {
     const className = 'input__select-arrow';
