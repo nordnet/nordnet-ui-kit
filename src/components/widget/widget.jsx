c4b39f9262ddab2626e7f67778114c408f6611d5
Gustaf Zetterlund <gustaf.zetterlund@gmail.com>
2016-04-27 18:27:37 +0200

    refactored widget component and added docs

--- src/components/widget/widget.jsx
+++ src/components/widget/widget.jsx
@@ -2,24 +2,37 @@ import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import './widget.scss';
 
-const Widget = props => (
-  <div { ...props } className={ classNames('widget', props.className) }>
-    { props.header ? props.header : <span /> }
-    <div className={ classNames('widget__body', { 'widget__body--full-width': props.fullWidth }) }>
-      { props.children }
-    </div>
-  </div>
-);
+function renderHeader(header) {
+  if (typeof header === 'string') {
+    return <h4 className="widget__heading">{ header }</h4>;
+  }
 
-Widget.defaltProps = {
+  return header;
+}
+
+export default function widget({ header, children, fullWidth, className, ...rest }) {
+  const classes = classNames('widget', className);
+  const bodyClasses = classNames('widget__body', {
+    'widget__body--full-width': fullWidth,
+  });
+
+  return (
+    <div { ...rest } className={ classes }>
+      <div className="widget__header">{ renderHeader(header) }</div>
+      <div className={ bodyClasses }>
+        { children }
+      </div>
+    </div>
+  );
+}
+
+widget.defaltProps = {
   fullWidth: false,
 };
 
-Widget.propTypes = {
+widget.propTypes = {
   children: PropTypes.node,
   header: PropTypes.node,
   className: PropTypes.string,
   fullWidth: PropTypes.bool,
 };
-
-export default Widget;
