9af0e8355cff7e4dea891482c15d87ce972c55d8
Robin Engström <robin.engstrom@nordnet.se>
2016-06-03 16:39:42 +0200

    modified spark graph to fill parent but keep specified strokeWidth if height or width is not set

--- src/components/spark-graph/spark-graph.jsx
+++ src/components/spark-graph/spark-graph.jsx
@@ -1,74 +1,115 @@
 import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import classNames from 'classnames';
 import variables from '../../utilities/variables';
 import './spark-graph.scss';
 
-function transformPoints(points, height, strokeWidth) {
-  const effectiveHeight = height - strokeWidth;
-  const horizontalPadding = strokeWidth / 2;
-  const { min, max } = getMinMax(points);
-  if (min === max) {
-    // The line must be exactly horizontal, normalization not possible
-    return [height / 2, height / 2];
+
+export default class SparkGraph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+      height: 0,
+    };
+    this.handleResize = this.handleResize.bind(this);
   }
-  return points.map(point => horizontalPadding + effectiveHeight -
-    (((point - min) / (max - min)) * effectiveHeight));
-}
 
-function getMinMax(points) {
-  return {
-    min: Math.min(...points),
-    max: Math.max(...points),
-  };
-}
-
-function buildPathString(points, width) {
-  const xInc = width / (points.length - 1);
-  return `M 0 ${points[0]} ${points.slice(1).reduce((prev, curr, index) =>
-    `${prev} L ${(index + 1) * xInc} ${curr}`, '')}`;
-}
-
-export default function SparkGraph({
-  points,
-  stroke,
-  strokeWidth,
-  width,
-  height,
-  className,
-  style,
-}) {
-  const classes = classNames('spark-graph', className);
-  const styles = Object.assign({
-    width: `${width}px`,
-    height: `${height}px`,
-  }, style);
-  let actualStroke = stroke;
-  if (!stroke) {
-    if (points[0] > points[points.length - 1]) {
-      actualStroke = variables.colorDanger;
-    } else if (points[0] < points[points.length - 1]) {
-      actualStroke = variables.colorSuccess;
-    } else {
-      actualStroke = variables.colorText;
+  componentDidMount() {
+    if (!this.props.height || !this.props.width) {
+      this.handleResize();
+      window.addEventListener('resize', this.handleResize);
     }
   }
-  const transformedPoints = transformPoints(points, height, strokeWidth);
-  return (
-    <svg
-      className={ classes }
-      style={ styles }
-      viewBox={ `0 0 ${width} ${height}` }
-    >
-      <path
-        className="spark-graph__path"
-        d={ buildPathString(transformedPoints, width) }
-        stroke={ actualStroke }
-        strokeWidth={ strokeWidth }
-        strokeLinecap="square"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
+
+  componentWillUnmount() {
+    if (!this.props.height || !this.props.width) {
+      window.removeEventListener('resize', this.handleResize);
+    }
+  }
+
+  getMinMax(points) {
+    return {
+      min: Math.min(...points),
+      max: Math.max(...points),
+    };
+  }
+
+  handleResize() {
+    const rect = ReactDOM.findDOMNode(this).getBoundingClientRect();
+    if (rect.height !== this.state.height || rect.width !== this.state.width) {
+      this.setState({
+        width: rect.width,
+        height: rect.height,
+      });
+    }
+  }
+
+  transformPoints(points, height, strokeWidth) {
+    const effectiveHeight = height - strokeWidth;
+    const horizontalPadding = strokeWidth / 2;
+    const { min, max } = this.getMinMax(points);
+    if (min === max) {
+      // The line must be exactly horizontal, normalization not possible
+      return [height / 2, height / 2];
+    }
+    return points.map(point => horizontalPadding + effectiveHeight -
+      (((point - min) / (max - min)) * effectiveHeight));
+  }
+
+  buildPathString(points, width) {
+    const xInc = width / (points.length - 1);
+    return `M 0 ${points[0]} ${points.slice(1).reduce((prev, curr, index) =>
+      `${prev} L ${(index + 1) * xInc} ${curr}`, '')}`;
+  }
+
+  render() {
+    const {
+      points,
+      stroke,
+      strokeWidth,
+      width,
+      height,
+      className,
+      style,
+      ...rest,
+    } = this.props;
+    const targetWidth = !width ? this.state.width : width;
+    const targetHeight = !height ? this.state.height : height;
+    const classes = classNames('spark-graph', className);
+    const styles = Object.assign({
+      width: !width ? '100%' : `${targetWidth}px`,
+      height: !height ? '100%' : `${targetHeight}px`,
+    }, style);
+    let actualStroke = stroke;
+    if (!stroke) {
+      if (points[0] > points[points.length - 1]) {
+        actualStroke = variables.colorDanger;
+      } else if (points[0] < points[points.length - 1]) {
+        actualStroke = variables.colorSuccess;
+      } else {
+        actualStroke = variables.colorText;
+      }
+    }
+    const transformedPoints = this.transformPoints(points, targetHeight, strokeWidth);
+    return (
+      <svg
+        { ...rest }
+        className={ classes }
+        style={ styles }
+        viewBox={ `0 0 ${targetWidth} ${targetHeight}` }
+      >
+        <path
+          className="spark-graph__path"
+          d={ this.buildPathString(transformedPoints, targetWidth) }
+          stroke={ actualStroke }
+          strokeWidth={ strokeWidth }
+          strokeLinecap="square"
+          strokeLinejoin="round"
+        />
+      </svg>
+    );
+  }
 }
 
 SparkGraph.propTypes = {
@@ -79,38 +120,41 @@ SparkGraph.propTypes = {
     const points = props[propName];
     if (!points) {
       return new Error(
-        `Invalid prop ${propName} supplied to ${componentName}. 
+        `Invalid prop ${propName} supplied to ${componentName}.
         Points undefined`
       );
     }
     if (!Array.isArray(points)) {
       return new Error(
-        `Invalid prop ${propName} supplied to ${componentName}. 
+        `Invalid prop ${propName} supplied to ${componentName}.
         Points need to be array`
       );
     }
     if (points.length < 2) {
       return new Error(
-        `Invalid prop ${propName} supplied to ${componentName}. 
+        `Invalid prop ${propName} supplied to ${componentName}.
         Need more than 2 points`
       );
     }
     if (points.some(isNaN)) {
       return new Error(
-        `Invalid prop ${propName} supplied to ${componentName}. 
+        `Invalid prop ${propName} supplied to ${componentName}.
         Need to be an array of numbers`
       );
     }
     return null;
   },
+  /**
+    Stroke color
+  */
   stroke: PropTypes.string,
   strokeWidth: PropTypes.number,
   /**
-    Width in px
+    Width in px or leave blank to fill parent
   */
   width: PropTypes.number,
   /**
-    Height in px
+    Height in px or leave blank to fill parent
   */
   height: PropTypes.number,
   className: PropTypes.string,
@@ -118,7 +162,5 @@ SparkGraph.propTypes = {
 };
 
 SparkGraph.defaultProps = {
-  width: 128,
-  height: 32,
   strokeWidth: 1,
 };
--- src/components/spark-graph/spark-graph.md
+++ src/components/spark-graph/spark-graph.md
@@ -4,11 +4,28 @@
 
     const style = {
       marginRight: '1.6rem',
-      background: '#f5f5f5'
+      background: '#f5f5f5',
+      display: 'inline-block',
+      height: 32,
+      width: 128,
     };
 
     <div>
-      <SparkGraph points={ eric } style={ style } />
-      <SparkGraph points={ abb } style={ style } />
-      <SparkGraph points={ [1, 3, 2, 3, 1] } style= { style } />
+      <div style={ style }>
+        <SparkGraph points={ eric }/>
+      </div>
+      <div style={ style }>
+        <SparkGraph points={ abb } />
+      </div>
+      <div style={ style }>
+        <SparkGraph points={ [3, 1, 3, 1, 3] } />
+      </div>
+      <div style={ style }>
+        <SparkGraph points={ [3, 1, 3, 1, 3] } stroke="blue" strokeWidth={5} />
+      </div>
+      <div style={ style }>
+        <SparkGraph points={ [3, 1, 3, 1, 3] } stroke="purple" strokeWidth={10}/>
+      </div>
+      <SparkGraph points={ [3, 1, 3, 1, 3] } style={ style } />
+      <SparkGraph points={ abb } />
     </div>
