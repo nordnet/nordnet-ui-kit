68d1fc42935f361a81992676de4618c832f59e64
Jesper Winér <jesper.winer@nordnet.se>
2016-06-07 11:14:51 +0200

    MIG-747 First commit for Highcharts component

---
+++ src/components/highcharts/highcharts.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactHighcharts from 'react-highcharts';
+import './highcharts.scss';
+
+const Highcharts = (props) => <ReactHighcharts {...props} />;
+
+export default Highcharts;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Alert from './components/alert/alert';
 import Badge from './components/badge/badge';
 import Button from './components/button/button';
 import Dropdown from './components/dropdown/dropdown';
+import Highcharts from './components/highcharts/highcharts';
 import Icon from './components/icon/icon';
 import Input from './components/input/input';
 import Logo from './components/logo/logo';
@@ -12,4 +13,4 @@ import Value from './components/value/value';
 import DatePicker from './components/date-picker';
 import Widget from './components/widget/widget';
 
-export { Alert, Badge, Button, Dropdown, Icon, Input, Logo, Pane, Select, DatePicker, Value, Widget, variables };
+export { Alert, Badge, Button, Dropdown, Highcharts, Icon, Input, Logo, Pane, Select, DatePicker, Value, Widget, variables };
