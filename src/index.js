971912389f6773795cd7358145c4e2fde193addf
GU5TAF <gustaf.zetterlund@gmail.com>
2016-08-03 11:24:19 +0200

    added missing entrypoint for table component

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ import Dropdown from './components/dropdown/dropdown';
 import Graph from './components/graph';
 import Icon from './components/icon/icon';
 import Input from './components/input/input';
+import LabeledValue from './components/labeled-value/labeled-value';
 import Logo from './components/logo/logo';
 import Pane from './components/pane/pane';
 import RatioBar from './components/ratio-bar';
 import Select from './components/select/select';
 import SparkGraph from './components/spark-graph';
 import Spinner from './components/spinner';
-import LabeledValue from './components/labeled-value/labeled-value';
+import Table from './components/table/';
 import variables from './utilities/variables';
 import Widget from './components/widget/widget';
 
@@ -25,13 +26,14 @@ export {
   Graph,
   Icon,
   Input,
+  LabeledValue,
   Logo,
   Pane,
   RatioBar,
   Select,
   SparkGraph,
   Spinner,
-  LabeledValue,
+  Table,
   variables,
   Widget,
 };
