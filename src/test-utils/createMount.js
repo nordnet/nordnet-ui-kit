9106cb3ca57d38ed80ba54ab4ea6509f048ad2f6
Andreas Broström <andreas.brostrom@nordnet.se>
2017-03-23 08:34:21 +0100

    Add mount test helper

---
+++ src/test-utils/createMount.js
@@ -0,0 +1,43 @@
+import { PropTypes } from 'react';
+import { mount as enzymeMount } from 'enzyme';
+import { ThemeProvider } from '../styles';
+
+function cleanStyles() {
+  const head = window.document.head;
+  const length = head.children.length;
+  for (let i = length - 1; i >= 0; i -= 1) {
+    if (head.children[i].tagName.toLowerCase() === 'style') {
+      head.removeChild(head.children[i]);
+    }
+  }
+}
+
+export default function createMount(mount = enzymeMount) {
+  cleanStyles();
+
+  const attachTo = window.document.createElement('div');
+  attachTo.className = 'app';
+  attachTo.setAttribute('id', 'app');
+  window.document.body.insertBefore(attachTo, window.document.body.firstChild);
+  const { theme, styleManager } = ThemeProvider.createDefaultContext();
+  const context = { theme, styleManager };
+  const childContextTypes = {
+    theme: PropTypes.object,
+    styleManager: PropTypes.object,
+  };
+
+  const mountWithContext = function mountWithContext(node) {
+    return mount(node, { context, attachTo, childContextTypes });
+  };
+
+  mountWithContext.context = context;
+  mountWithContext.attachTo = attachTo;
+
+  mountWithContext.cleanUp = () => {
+    styleManager.reset();
+    cleanStyles();
+    attachTo.parentNode.removeChild(attachTo);
+  };
+
+  return mountWithContext;
+}
