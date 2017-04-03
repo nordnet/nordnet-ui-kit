4676be201e17c4c0b86723234d4fc987af6250e5
Vladimir Starkov <iamstarkov@gmail.com>
2017-04-03 19:56:35 +0200

    redo Badge

--- src/components/badge/badge.jsx
+++ src/components/badge/badge.jsx
@@ -1,52 +1,27 @@
 import React, { PropTypes } from 'react';
 import cn from 'classnames';
-import { createStyleSheet } from 'jss-theme-reactor';
-
-export const styleSheet = createStyleSheet('Badge', (theme) => {
-  const { palette } = theme;
-
-  return {
-    root: {
-      display: 'inline-block',
-      fontSize: '12px',
-      padding: '2px 8px',
-      lineHeight: 1,
-      color: palette.shades.dark.text.default,
-      textAlign: 'center',
-      whiteSpace: 'nowrap',
-      verticalAlign: 'middle',
-      backgroundColor: palette.variant.primary,
-      borderRadius: '10px',
-    },
-    success: {
-      backgroundColor: palette.variant.success,
-    },
-    warning: {
-      backgroundColor: palette.variant.warning,
-      color: palette.text.default,
-    },
-    danger: {
-      backgroundColor: palette.variant.danger,
-    },
-  };
-});
+import { withTheme, injectSheet } from '../../';
+import styles from './styles';
 
 function Badge({
+  classes,
   modifier,
   children,
   className: classNameProp,
   ...rest
-}, { styleManager }) {
-  const classes = styleManager.render(styleSheet);
-
-  const className = cn([classes.root], {
-    [classes.success]: modifier === 'success',
-    [classes.warning]: modifier === 'warning',
-    [classes.danger]: modifier === 'danger',
-  }, classNameProp);
-
+}) {
   return (
-    <span {...rest} className={className}>{ children }</span>
+    <span
+      className={cn({
+        [classes.badge]: true,
+        [classes.success]: modifier === 'success',
+        [classes.warning]: modifier === 'warning',
+        [classes.danger]: modifier === 'danger',
+      }, classNameProp)}
+      {...rest}
+    >
+      { children }
+    </span>
   );
 }
 
@@ -54,10 +29,7 @@ Badge.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
   modifier: PropTypes.oneOf(['success', 'warning', 'danger']),
+  classes: PropTypes.object.isRequired,
 };
 
-Badge.contextTypes = {
-  styleManager: PropTypes.object.isRequired,
-};
-
-export default Badge;
+export default withTheme(injectSheet(styles)(Badge));
--- src/components/badge/badge.md
+++ src/components/badge/badge.md
@@ -1,10 +1,14 @@
+    const { provideTheme, createTheme } = require('../../'); // 'nordnet-ui-kit'
+    const App = ({ children }) => (<div>{ children }</div>);
+    const AppWithTheme = provideTheme(createTheme(), App);
+
     const style = {
       marginRight: '1.6rem',
     };
 
-    <div>
+    <AppWithTheme>
       <Badge style={ style }>Pending</Badge>
       <Badge modifier="danger" style={ style }>Danger</Badge>
       <Badge modifier="warning" style={ style }>Warning</Badge>
       <Badge modifier="success" style={ style }>Success</Badge>
-    </div>
+    </AppWithTheme>
