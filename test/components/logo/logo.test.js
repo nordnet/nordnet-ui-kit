e5e80466de4ccc2b27776d114d0fc0f2f5b8b73e
Wilhelm Eklund <wille.eklund@gmail.com>
2017-04-04 14:35:20 +0200

    Fix the unit test for Logo component

--- test/components/logo/logo.test.js
+++ test/components/logo/logo.test.js
@@ -14,25 +14,17 @@ describe('<Logo />', () => {
     expect(wrapper.type()).to.equal('span');
   });
 
-  it('should have class logo', () => {
-    expect(wrapper.hasClass('logo')).to.equal(true);
+  it('should have a default width', () => {
+    expect(wrapper.prop('style').width).to.equal(130);
   });
 
-  it('should have class logo--default if no type is specified', () => {
-    expect(wrapper.hasClass('logo--default')).to.equal(true);
+  it('should be possible to customize the width', () => {
+    let newElement = shallow(<Logo width={200} />);
+    expect(newElement.prop('style').width).to.equal(200);
   });
 
   it('should have type default if no type is specified', () => {
     expect(wrapper.find('span[type="default"]')).to.have.length(1);
   });
 
-  it('should have class logo--something if type "something" is specified', () => {
-    wrapper.setProps({ type: 'something' });
-    expect(wrapper.hasClass('logo--something')).to.equal(true);
-  });
-
-  it('should have type "something" if type "something" is specified', () => {
-    wrapper.setProps({ type: 'something' });
-    expect(wrapper.find('span[type="something"]')).to.have.length(1);
-  });
 });
