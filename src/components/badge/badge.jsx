ad346775cd86686b1e576f815b5902960f8c4f41
Andreas Broström <andreas.brostrom@nordnet.se>
2017-04-04 13:47:25 +0200

    Rework badge with dynamic bgc and color based on contrast

--- src/components/badge/badge.jsx
+++ src/components/badge/badge.jsx
@@ -1,25 +1,35 @@
 import React, { PropTypes } from 'react';
 import cn from 'classnames';
+import Color from 'color';
 import { withTheme, injectSheet } from '../../';
-import styles from './styles';
 
-function Badge({
-  classes,
-  modifier,
-  children,
-  className: classNameProp,
-  ...rest
-}) {
+// TODO: Move these UTILS to styles
+const CONTRAST_THRESHOLD = 5;
+const getContrastRatio = (c1, c2) => Color(c1).contrast(Color(c2));
+const isContrast = (c1, c2, threshold = CONTRAST_THRESHOLD) => getContrastRatio(c1, c2) >= threshold;
+const getTextColor = (theme, light) => theme.palette.shades[light ? 'light' : 'dark'].text.default;
+
+const bgc = ({ theme, modifier }) => theme.palette.variant[modifier || 'primary'];
+const col = p => getTextColor(p.theme, isContrast(bgc(p), getTextColor(p.theme, true)));
+
+export const styles = {
+  badge: {
+    display: 'inline-block',
+    fontSize: 12,
+    padding: '.15em .5em',
+    lineHeight: 1,
+    color: col,
+    textAlign: 'center',
+    whiteSpace: 'nowrap',
+    verticalAlign: 'middle',
+    'background-color': bgc,
+    borderRadius: '1em',
+  },
+};
+
+function Badge({ classes, children, className, style }) {
   return (
-    <span
-      className={cn({
-        [classes.badge]: true,
-        [classes.success]: modifier === 'success',
-        [classes.warning]: modifier === 'warning',
-        [classes.danger]: modifier === 'danger',
-      }, classNameProp)}
-      {...rest}
-    >
+    <span style={style} className={cn(classes.badge, className)}>
       { children }
     </span>
   );
@@ -28,8 +38,9 @@ function Badge({
 Badge.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
-  modifier: PropTypes.oneOf(['success', 'warning', 'danger']),
+  modifier: PropTypes.oneOf(['success', 'warning', 'danger']), // eslint-disable-line react/no-unused-prop-types
   classes: PropTypes.object.isRequired,
+  style: PropTypes.object,
 };
 
 export default withTheme(injectSheet(styles)(Badge));
--- src/components/badge/badge.md
+++ src/components/badge/badge.md
@@ -1,14 +1,10 @@
-    const { provideTheme, createTheme } = require('../../'); // 'nordnet-ui-kit'
-    const App = ({ children }) => (<div>{ children }</div>);
-    const AppWithTheme = provideTheme(createTheme(), App);
-
     const style = {
-      marginRight: '1.6rem',
+      marginRight: 16,
     };
 
-    <AppWithTheme>
+    <div>
       <Badge style={ style }>Pending</Badge>
       <Badge modifier="danger" style={ style }>Danger</Badge>
       <Badge modifier="warning" style={ style }>Warning</Badge>
       <Badge modifier="success" style={ style }>Success</Badge>
-    </AppWithTheme>
+    </div>
