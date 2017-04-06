134d60e3c8bb2d09788eaa69f4e82cfdc44a92ce
Vladimir Starkov <iamstarkov@gmail.com>
2017-04-06 19:29:11 +0200

    add decorateShallow

--- src/test-utils/index.js
+++ src/test-utils/index.js
@@ -2,8 +2,12 @@ import createMount from './createMount';
 import createShallow from './createShallow';
 import createRenderToString from './createRenderToString';
 
+import decorateShallow from './decorateShallow';
+
 export {
   createMount,
   createShallow,
   createRenderToString,
+
+  decorateShallow,
 };
