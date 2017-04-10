089e9182392578db300ece9346415dd47139ee00
Wilhelm Eklund <wille.eklund@gmail.com>
2017-04-10 17:59:07 +0200

    Fix stroke colors for ValidationIcon component

--- src/components/input/validation-icon.jsx
+++ src/components/input/validation-icon.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Icon } from '../../';
-// import variables from '../../utilities/variables';
+import color from '../../styles/color';
 
 function ValidationIcon(props) {
   const iconClass = 'input__validation-icon';
 
   if (props.hasSuccess) {
-    return <Icon.Checkmark className={iconClass} />;
+    return <Icon.Checkmark className={iconClass} stroke={color.green} />;
   }
 
   if (props.hasWarning) {
-    return <Icon.ExclamationPoint className={iconClass} stroke={'variables.colorWarning'} />;
+    return <Icon.ExclamationPoint className={iconClass} stroke={color.yellow} />;
   }
 
   if (props.hasError) {
-    return <Icon.ExclamationPoint className={iconClass} />;
+    return <Icon.ExclamationPoint className={iconClass} stroke={color.red} />;
   }
 
   return null;
