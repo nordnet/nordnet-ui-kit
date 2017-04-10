0ec17df864f925ad62fdbf46082a5d3bfa12a838
Andreas Broström <andreas.brostrom@nordnet.se>
2017-04-10 08:47:04 +0200

    Fast porting RadioGroup, RangeSelector, RatioBar and Select

--- src/components/ratio-bar/ratio-bar.jsx
+++ src/components/ratio-bar/ratio-bar.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
-import './ratio-bar.scss';
+// import './ratio-bar.scss';
 
 function percentage(value, total) {
   return (value / total) * 100;
