72163d4a287b574862fd6e0a1643d284297648dd
Andreas Broström <andreas.brostrom@nordnet.se>
2017-04-24 17:34:55 +0200

    Change mixins

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -1,13 +1,19 @@
-export default function createMixins(breakpoints) {
-  // TODO use breakpoints
+const basicBoxSizing = {
+  '&, & *, & *::after, & *::after': {
+    boxSizing: 'border-box',
+  },
+};
 
-  const basicBoxSizing = {
-    '&, & *, & *::after, & *::after': {
-      boxSizing: 'border-box',
-    },
+export default function createMixins(breakpoints = {}) {
+  const media = (size) => {
+    const breakpoint = breakpoints[size];
+
+    if (breakpoint) {
+      return `@media only screen and (min-width: ${breakpoints[size]}px)`;
+    }
+
+    return '&';
   };
-
-  const media = (size = 'xs') => `@media only screen and (min-width: ${breakpoints[size]})`;
 
   return {
     basicBoxSizing,
