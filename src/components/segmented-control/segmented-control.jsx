bcb50d8baf134e89c03f9a8e93e655ad6fe7ebe6
Wilhelm Eklund <wille.eklund@gmail.com>
2017-04-24 14:02:14 +0200

    Added option on segmented control to work as a toggle button with single child

--- src/components/segmented-control/segmented-control.jsx
+++ src/components/segmented-control/segmented-control.jsx
@@ -5,8 +5,9 @@ import SegmentedControlStyles from './segmented-control-styles';
 class SegmentedControl extends React.PureComponent {
   constructor(props, context) {
     super(props, context);
-    this.state = { selected: props.value };
+    this.state = { selected: props.value, toggleValue: props.toggleValue };
     this.handleChange = this.handleChange.bind(this);
+    this.toggleStateValue = this.toggleStateValue.bind(this);
     this.classes = this.context.styleManager.render(SegmentedControlStyles);
   }
 
@@ -21,31 +22,59 @@ class SegmentedControl extends React.PureComponent {
     });
   }
 
+  toggleStateValue() {
+    const toggleValue = !this.state.toggleValue;
+    this.setState({
+      toggleValue,
+    }, () => {
+      if (this.props.onChange) {
+        this.props.onChange(toggleValue);
+      }
+    });
+  }
+
   render() {
+    // If we have an array of children, then we have the map function
+    if (this.props.children.map) {
+      return (
+        <span style={this.props.style} className={cn(this.classes.root, this.props.className)}>
+          { this.props.children.map((child, index) => {
+            const childValue = (child.props || {}).value;
+            const selected = childValue === this.state.selected || index === this.state.selected;
+            const usedClassName = cn(this.classes.label, {
+              [this.classes.selected]: selected,
+              [this.classes.buttonLeft]: index === 0,
+              [this.classes.buttonRight]: index === this.props.children.length - 1,
+            });
+            const inputId = `sc-${this.props.name}-${index}`;
+            return (<span className={usedClassName} key={`sc-${this.props.name}-${childValue}`}>
+              <input
+                type="radio"
+                name={`sc-${this.props.name}`}
+                id={inputId}
+                value={childValue || index}
+                checked={selected}
+                onChange={this.handleChange}
+              />
+              <label htmlFor={inputId}>{ child }</label>
+            </span>);
+          }) }
+        </span>
+      );
+    }
+    // If single node as child
+    const selected = this.state.toggleValue;
+    const usedClassName = cn(this.classes.root, this.classes.label, this.classes.buttonLeft, this.classes.buttonRight, {
+      [this.classes.selected]: selected,
+    }, this.props.className);
     return (
-      <span style={this.props.style} className={cn(this.classes.root, this.props.className)}>
-        { this.props.children.map((child, index) => {
-          const childValue = (child.props || {}).value;
-          const selected = childValue === this.state.selected || index === this.state.selected;
-          const usedClassName = cn(this.classes.label, {
-            [this.classes.selected]: selected,
-            [this.classes.buttonLeft]: index === 0,
-            [this.classes.buttonRight]: index === this.props.children.length - 1,
-          });
-          const inputId = `sc-${this.props.name}-${index}`;
-          return (<span className={usedClassName} key={`sc-${this.props.name}-${childValue}`}>
-            <input
-              type="radio"
-              name={`sc-${this.props.name}`}
-              id={inputId}
-              value={childValue || index}
-              checked={selected}
-              onChange={this.handleChange}
-            />
-            <label htmlFor={inputId}>{ child }</label>
-          </span>);
-        }) }
-      </span>
+      <button
+        style={this.props.style}
+        className={usedClassName}
+        onClick={this.toggleStateValue}
+      >
+        {this.props.children}
+      </button>
     );
   }
 }
@@ -57,6 +86,7 @@ SegmentedControl.propTypes = {
   style: PropTypes.object,
   name: PropTypes.string,
   value: PropTypes.string,
+  toggleValue: PropTypes.bool,
 };
 
 SegmentedControl.contextTypes = {
--- src/components/segmented-control/segmented-control.md
+++ src/components/segmented-control/segmented-control.md
@@ -1,7 +1,16 @@
 
     <SegmentedControl
-      onChange={ newValue => { console.debug('new value', newValue); } }
+      onChange={ newValue => { console.log('new segment value', newValue); } }
       value="green" name="color">
       <span value="red">Red</span>
       <span value="green">Green</span>
+    </SegmentedControl>
+
+
+With single option (a toggler):
+
+    <SegmentedControl
+      onChange={ newValue => { console.log('new toggle value', newValue); } }
+      name="toggler" toggleValue={true}>
+      <span value="red">Red</span>
     </SegmentedControl>
