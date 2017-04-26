2895d23c2362c4fc5784e28f7d58805b79c82082
Jesper Annebäck <jesper.anneback@nordnet.se>
2017-04-26 18:07:11 +0200

    Readded pane component

---
+++ src/components/pane/pane.jsx
@@ -0,0 +1,97 @@
+/* eslint jsx-a11y/no-static-element-interactions: 0, react/no-array-index-key: 0 */
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { kebabCase } from 'lodash';
+import PaneStyles from './pane-styles';
+
+class Pane extends React.PureComponent {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      activeTab: props.activeTab || 0,
+    };
+
+    this.handleTabClick = this.handleTabClick.bind(this);
+
+    this.classes = this.context.styleManager.render(PaneStyles);
+  }
+
+  handleTabClick(index) {
+    this.setState({
+      activeTab: index,
+    });
+  }
+
+  renderTabs() {
+    const tabWidth = 100 / this.props.tabs.length;
+
+    return (
+      <ul className={this.classes.tabs}>
+        { this.props.tabs.map((tab, index) => (
+          <li
+            key={`${kebabCase(tab.label)}_${index}`}
+            className={classNames(
+              this.classes.tab,
+              this.classes[this.props.size],
+              { [this.classes.active]: this.state.activeTab === index },
+            )}
+            style={{ width: `${tabWidth}%` }}
+            onClick={() => this.handleTabClick(index)}
+          >
+            { tab.label }
+          </li>
+        )) }
+      </ul>
+    );
+  }
+
+  renderBody() {
+    if (this.props.tabs && this.props.tabs.length > 0) {
+      return (
+        <div>
+          { this.props.tabs.map((tab, index) => (
+            <div key={`${kebabCase(tab.label)}_body_${index}`} style={{ display: this.state.activeTab === index ? 'block' : 'none' }}>
+              { tab.body }
+            </div>
+          )) }
+        </div>
+      );
+    }
+    return null;
+  }
+
+  render() {
+    const className = classNames(this.classes.pane, this.props.className);
+    if (this.props.tabs && this.props.tabs.length > 0) {
+      return (
+        <div className={className}>
+          { this.renderTabs() }
+          { this.renderBody() }
+        </div>
+      );
+    }
+    return null;
+  }
+}
+
+Pane.propTypes = {
+  className: PropTypes.string,
+  activeTab: PropTypes.number,
+  tabs: PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.node,
+    body: PropTypes.node,
+  })),
+  size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg']),
+};
+
+Pane.defaultProps = {
+  size: 'md',
+};
+
+Pane.contextTypes = {
+  styleManager: PropTypes.object.isRequired,
+};
+
+
+export default Pane;
---
+++ test/components/pane/pane.test.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow as enzymeShallow } from 'enzyme';
+import { createShallow } from '../../../src/test-utils';
+import Pane from '../../../src/components/pane/pane';
+import PaneStyles from '../../../src/components/pane/pane-styles';
+
+describe('<Pane />', () => {
+  const shallow = createShallow(enzymeShallow);
+  const classes = shallow.context.styleManager.render(PaneStyles);
+
+  describe('When <Pane /> don´t have any tabs', () => {
+    const wrapper = shallow(<Pane />);
+    it('should render null', () => {
+      expect(wrapper.type()).to.equal(null);
+    });
+  });
+
+  describe('When <Pane /> is correctly populated with default props', () => {
+    const tabs = [{
+      label: 'Tab 1',
+      body: <div>This is tab 1!</div>,
+    }, {
+      label: 'Tab 2',
+      body: <div>This is tab 2!</div>,
+    }, {
+      label: 'Tab 3',
+      body: <div>This is tab 3!</div>,
+    }];
+
+    let wrapper;
+    let renderedTabs;
+
+    beforeEach(() => {
+      wrapper = shallow(<Pane tabs={tabs} />);
+      renderedTabs = wrapper.children().children().find('li');
+    });
+
+    it('should render <div> as container', () => {
+      expect(wrapper.type()).to.equal('div');
+    });
+
+    it('should have the class pane', () => {
+      expect(wrapper.hasClass(classes.pane)).to.equal(true);
+    });
+
+    it('should render children', () => {
+      expect(wrapper.children().length).to.equal(2);
+    });
+
+    it('should render tabs', () => {
+      expect(renderedTabs.length).to.equal(3);
+    });
+
+    it('should have first tab as active', () => {
+      expect(renderedTabs.at(0).hasClass(classes.active));
+    });
+
+    it('default size should be md', () => {
+      expect(renderedTabs.at(0).hasClass(classes.md)).to.equal(true);
+    });
+  });
+
+  describe('When <Pane /> is correctly populated with another size', () => {
+    const tabs = [{
+      label: 'Tab 1',
+      body: <div>This is tab 1!</div>,
+    }];
+    const wrapper = shallow(<Pane tabs={tabs} size="lg" />);
+    it('should render <div> as container', () => {
+      expect(wrapper.type()).to.equal('div');
+    });
+
+    it('should have the class pane', () => {
+      expect(wrapper.hasClass(classes.pane)).to.equal(true);
+    });
+
+    it('should have class lg', () => {
+      expect(wrapper.children().children().find('li').at(0)
+      .hasClass(classes.lg)).to.equal(true);
+    });
+  });
+});
