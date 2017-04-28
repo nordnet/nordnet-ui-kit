4bbe22a11a96f482aae59232031b44f0ed5a5cbc
Andreas Broström <andreas.brostrom@nordnet.se>
2017-04-28 13:57:43 +0200

    Tweak social icons

--- src/components/icon/icons/socialFacebook.jsx
+++ src/components/icon/icons/socialFacebook.jsx
@@ -10,12 +10,10 @@ export default function SocialFacebook({
   return (
     <svg {...rest}>
       <g id="Page-1" stroke="none" strokeWidth={strokeWidth} fill="none" fillRule="evenodd">
-        <g id="Artboard-1" transform="translate(-1056.000000, -282.000000)" stroke={stroke} strokeWidth={strokeWidth}>
-          <g id="social-facebook" transform="translate(1056.000000, 282.000000)">
-            <rect id="Rectangle-path" x="1" y="1" width="14" height="14" />
-            <path d="M12,5 L11,5 C9.9,5 9,5.9 9,7 L9,14" id="Shape" />
-            <path d="M6,9 L12,9" id="Shape" fill={fill} />
-          </g>
+        <g id="social-facebook" stroke={stroke} strokeWidth={strokeWidth}>
+          <rect id="Rectangle-path" x="1" y="1" width="14" height="14" />
+          <path d="M12,5 L11,5 C9.9,5 9,5.9 9,7 L9,14" id="Shape" />
+          <path d="M6,9 L12,9" id="Shape" fill={fill} />
         </g>
       </g>
     </svg>
