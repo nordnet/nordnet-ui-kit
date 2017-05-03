ef6e6d9f7e83b69544a2ac1e4017157479d33b96
Andreas Broström <andreas.brostrom@nordnet.se>
2017-05-03 10:22:59 +0200

    Remove SparkGraph and prepare 0.2.0

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import Li from './components/li/li';
 import Logo from './components/logo/logo';
 import Pane from './components/pane/pane';
 import RadioGroup from './components/radio-group';
-// import SparkGraph from './components/spark-graph';
 import Spinner from './components/spinner';
 import Table from './components/table';
 import Tbody from './components/tbody';
@@ -155,7 +154,6 @@ export {
   Logo,
   Pane,
   RadioGroup,
-  // SparkGraph,
   Spinner,
   Table,
   Tbody,
