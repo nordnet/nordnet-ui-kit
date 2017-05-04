e6f8ae5f9825225d8260e809145e2051a90572d2
Andreas Broström <andreas.brostrom@nordnet.se>
2017-05-04 14:34:20 +0200

    Add round option to flags

---
+++ src/components/flag/flags/currencies/index.js
@@ -0,0 +1,15 @@
+import euDk from './eu-dk';
+import euNo from './eu-no';
+import euSe from './eu-se';
+import dkSe from './dk-se';
+import noSe from './no-se';
+import usSe from './us-se';
+
+export default {
+  'eu-dk': euDk,
+  'eu-no': euNo,
+  'eu-se': euSe,
+  'dk-se': dkSe,
+  'no-se': noSe,
+  'us-se': usSe,
+};
---
+++ src/components/flag/flags/index.js
@@ -0,0 +1,14 @@
+import ca from './ca';
+import de from './de';
+import fr from './fr';
+import ru from './ru';
+import gb from './gb';
+import dk from './dk';
+import fi from './fi';
+import no from './no';
+import se from './se';
+import us from './us';
+import jp from './jp';
+import cn from './cn';
+
+export default { ca, de, fr, ru, gb, dk, fi, no, se, us, jp, cn };
