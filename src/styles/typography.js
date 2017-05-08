67fb3203c5e4b7f1da5ee0895545c4b7c63b266f
Andreas Broström <bstream@users.noreply.github.com>
2017-05-08 17:07:25 +0200

    Feature/extend typography (#202)
    * Extend font families and add more font weights
    
    * Re-add medium to avoid breaking change, but change to valid value

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,10 +1,13 @@
 const defaultConstants = {
-  fontFamilyOpen: '"Open Sans"',
-  fontFamilyBitter: 'Bitter',
+  fontFamilyOpen: '"Open Sans", sans-serif',
+  fontFamilyBitter: 'Bitter, serif',
   fontSize: 16,
   fontWeightLight: 300,
   fontWeightRegular: 400,
-  fontWeightMedium: 500,
+  fontWeightMedium: 400,
+  fontWeightSemiBold: 600,
+  fontWeightBold: 700,
+  fontWeightExtraBold: 800,
 };
 
 export default function createTypography(constants = defaultConstants) {
