a9a734c1f8d5b635f823dd4f877c609a3a729cce
Wilhelm Eklund <wille.eklund@gmail.com>
2017-05-11 13:38:47 +0200

    Avoid id collision with the flags (#209)
    Prepending the ids used in the flags with the name of the flag.
    This solves #207.

--- src/components/flag/flags/jp.jsx
+++ src/components/flag/flags/jp.jsx
@@ -6,11 +6,11 @@ export default function FlagJp({
   return (
     <svg aria-hidden="true" focusable="false" {...rest}>
       <defs>
-        <clipPath id="a">
+        <clipPath id="jp_a">
           <path fillOpacity=".67" d="M-88.001 32h640v480h-640z" />
         </clipPath>
       </defs>
-      <g fillRule="evenodd" clipPath="url(#a)" transform="translate(88.001 -32)" strokeWidth="1pt">
+      <g fillRule="evenodd" clipPath="url(#jp_a)" transform="translate(88.001 -32)" strokeWidth="1pt">
         <path fill="#fff" d="M-128 32h720v480h-720z" />
         <ellipse rx="194.93" ry="194.93" transform="translate(-168.44 8.618) scale(.76554)" cy="344.05" cx="523.08" fill="#d30000" />
       </g>
