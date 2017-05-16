8d616d6808d79955b57a341e518a3070318dfd9c
Andreas Broström <andreas.brostrom@nordnet.se>
2017-05-16 14:39:45 +0200

    test: Fixing setup jsdom that prettier broke 😭

--- test/setup.jsdom.js
+++ test/setup.jsdom.js
@@ -1,7 +1,6 @@
 const jsdom = require('jsdom');
 
-global.document = jsdom.jsdom(
-  '<html><head><script></script></head><body><div id="app"></div></body></html>',
-);
+// prettier-ignore
+global.document = jsdom.jsdom('<html><head><script></script></head><body><div id="app"></div></body></html>');
 global.window = document.defaultView;
 global.navigator = global.window.navigator;
