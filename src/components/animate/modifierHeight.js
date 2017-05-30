1862cf93eda21d5ea6525463447d2eeb697cd215
Wilhelm Eklund <wille.eklund@gmail.com>
2017-05-30 17:48:37 +0200

    fix(Animate): Add extra div around the children in Animate
    This solves the problem when the children has top or bottom padding. Now we should only have single
    child in each Animate.

--- src/components/animate/modifierHeight.js
+++ src/components/animate/modifierHeight.js
@@ -11,7 +11,12 @@ export default ({
   easingLeaveFunction = easings.easeOut,
 }) => ({
   overflow: 'hidden',
-  maxHeight: 'none',
+  boxSizing: 'border-box',
+
+  '& > div': {
+    overflow: 'hidden',
+    boxSizing: 'border-box',
+  },
 
   [`&${classPrefixSpace ? ' ' : ''}.${name}`]: {
     '&-enter': {
