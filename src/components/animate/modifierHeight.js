3140735e5dda5e7bc241cf7c466acb00cb670f3f
Wilhelm Eklund <wille.eklund@gmail.com>
2017-06-13 12:54:52 +0200

    fix(Animate): Use height as well as max-height when animating (#244)
    This makes the animation stutter less in Firefox. Less work calculating reflows during animation.

--- src/components/animate/modifierHeight.js
+++ src/components/animate/modifierHeight.js
@@ -15,22 +15,26 @@ export default ({
 
   [`&${classPrefixSpace ? ' ' : ''}.${name}`]: {
     '&-enter': {
+      height: 0,
       maxHeight: 0,
 
       [`&.${name}-enter-active`]: {
+        height: estimatedHeight,
         maxHeight: estimatedHeight,
         transitionTimingFunction: easingEnterFunction,
-        transitionProperty: 'max-height',
+        transitionProperty: 'max-height, height',
         transitionDuration: transitionEnterTimeout,
       },
     },
     '&-leave': {
+      height: estimatedHeight,
       maxHeight: estimatedHeight,
       transitionTimingFunction: easingLeaveFunction,
-      transitionProperty: 'max-height',
+      transitionProperty: 'max-height, height',
       transitionDuration: transitionLeaveTimeout,
 
       [`&.${name}-leave-active`]: {
+        height: 0,
         maxHeight: 0,
       },
     },
