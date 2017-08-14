cebe7990e30373b4861e0a8aa71a7d2f3afae811
Vladimir Starkov <iamstarkov@gmail.com>
2017-08-14 09:10:59 +0200

    feat: add mockClasses test utility

--- src/test-utils/index.js
+++ src/test-utils/index.js
@@ -1,5 +1,6 @@
 import createMount from './createMount';
 import createShallow from './createShallow';
 import createRenderToString from './createRenderToString';
+import mockClasses from './mock-classes';
 
-export { createMount, createShallow, createRenderToString };
+export { createMount, createShallow, createRenderToString, mockClasses };
