df910e095c7aa5f653048fef852e088f02d43842
Andreas Broström <andreas.brostrom@nordnet.se>
2017-08-16 16:16:41 +0200

    fix(ThemeProvider): Add stripIndent import to avoid crash

--- src/styles/ThemeProvider.jsx
+++ src/styles/ThemeProvider.jsx
@@ -1,10 +1,11 @@
 import { createThemeProvider } from '@iamstarkov/jss-theme-reactor/ThemeProvider';
+import { stripIndent } from 'common-tags';
 import { createTheme } from './theme';
 
 // https://github.com/RikardGehlin/jss-theme-reactor/blob/master/src/ThemeProvider.js
 export default (...args) => {
   if (process.env.NODE_ENV !== 'production') {
-    // eslint-disable-next-line
+    // eslint-disable-next-line no-console
     console.warn(stripIndent`
       nordnet-ui-kit's \`ThemeProvider\` is deprecated and will be removed in major release.
       Please use react-jss's \`ThemeProvider\`.
