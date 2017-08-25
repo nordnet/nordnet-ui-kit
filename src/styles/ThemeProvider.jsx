125c73b15df379dcbaa909168826a33d71a556e1
Andreas Broström <andreas.brostrom@nordnet.se>
2017-08-25 11:16:08 +0200

    fix(ThemeProvider): Refactor so it does not print warning when not in use

--- src/styles/ThemeProvider.jsx
+++ src/styles/ThemeProvider.jsx
@@ -2,15 +2,19 @@ import { createThemeProvider } from '@iamstarkov/jss-theme-reactor/ThemeProvider
 import { stripIndent } from 'common-tags';
 import { createTheme } from './theme';
 
-// https://github.com/RikardGehlin/jss-theme-reactor/blob/master/src/ThemeProvider.js
-export default (function() {
-  if (process.env.NODE_ENV !== 'production') {
-    // eslint-disable-next-line no-console
-    console.warn(stripIndent`
-      nordnet-ui-kit's \`ThemeProvider\` is deprecated and will be removed in major release.
-      Please use react-jss's \`ThemeProvider\`.
-      See more https://github.com/nordnet/nordnet-ui-kit/releases/tag/v1.7.0
-    `);
+const JssThemeReactorThemeProvider = createThemeProvider(createTheme);
+
+export default class ThemeProvider extends JssThemeReactorThemeProvider {
+  constructor(props, context) {
+    super(props, context);
+
+    if (process.env.NODE_ENV !== 'production') {
+      // eslint-disable-next-line no-console
+      console.warn(stripIndent`
+        nordnet-ui-kit's \`ThemeProvider\` is deprecated and will be removed in major release.
+        Please use react-jss's \`ThemeProvider\`.
+        See more https://github.com/nordnet/nordnet-ui-kit/releases/tag/v1.7.0
+      `);
+    }
   }
-  return createThemeProvider(createTheme);
-})()
+}
