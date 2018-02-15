4207094d35341cdce2d53724865b0994cedf80b1
Andreas Broström <bstream@users.noreply.github.com>
2018-02-15 10:41:00 +0100

    Button fixes (#354)
    * fix(Button): Center text for `block` buttons
    
    * fix(Button): Reserve space for active/focus styles

--- src/components/button/button-styles.jsx
+++ src/components/button/button-styles.jsx
@@ -71,6 +71,7 @@ export default theme => {
       ...mixins.basicBoxSizing,
       display: 'inline-block',
       border: 0,
+      margin: 2,
       borderRadius: 16,
       fontFamily: typography.primary.fontFamily,
       lineHeight: 1,
@@ -186,6 +187,7 @@ export default theme => {
     innerWrapper: {
       alignItems: 'center',
       display: 'flex',
+      justifyContent: 'center',
     },
 
     spaceForIcon: {
