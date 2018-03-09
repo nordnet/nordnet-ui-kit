42315adfd5db40434ca8bd293cab72b58360e2a9
Mark Lenser <mlenser@gmail.com>
2018-03-09 09:50:33 +0100

    chore(badger): remove excess spacing

--- src/components/badge/badge.sketch.jsx
+++ src/components/badge/badge.sketch.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Badge from './badge';
 
-export const symbols = {
+const symbols = {
   'Badge/default': <Badge>Badge</Badge>,
-  'Badge/danger': <Badge modifier="danger" >Badge</Badge>,
-  'Badge/warning': <Badge modifier="warning" >Badge</Badge>,
-  'Badge/success': <Badge modifier="success" >Badge</Badge>,
+  'Badge/danger': <Badge modifier="danger">Badge</Badge>,
+  'Badge/warning': <Badge modifier="warning">Badge</Badge>,
+  'Badge/success': <Badge modifier="success">Badge</Badge>,
 };
+
+export default symbols;
