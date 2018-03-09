ebfc5725647a5a5300b39c4b75422a2b414a98bd
Mark Lenser <mlenser@gmail.com>
2018-03-09 10:40:59 +0100

    chore(input): set up input sketch

---
+++ src/components/input/input.sketch.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Input from './input';
+
+const symbols = {
+  'Input/text/default': <Input label="label" placeholder="placeholder" />,
+  'Input/text/secondary': <Input label="label" placeholder="placeholder" variant="secondary" />,
+  'Input/text/success': <Input label="label" placeholder="placeholder" hasSuccess />,
+  'Input/text/error': <Input label="label" placeholder="placeholder" hasError />,
+  'Input/text/warning': <Input label="label" placeholder="placeholder" hasWarning />,
+  'Input/text/leftAddon': <Input label="label" placeholder="placeholder" leftAddon="$" />,
+  'Input/text/rightAddon': <Input label="label" placeholder="placeholder" rightAddon="USD" />,
+  'Input/password': <Input type="password" label="label" placeholder="password" />,
+  'Input/select': (
+    <Input
+      type="select"
+      label="label"
+      placeholder="password"
+      options={[
+        {
+          label: '',
+          value: 1,
+        },
+      ]}
+    />
+  ),
+  'Input/checkbox': <Input type="checkbox" label="label" />,
+  'Input/checkbox/checked': <Input type="checkbox" label="label" checked />,
+  'Input/checkbox/success': <Input type="checkbox" label="label" hasSuccess />,
+  'Input/checkbox/error': <Input type="checkbox" label="label" hasError />,
+  'Input/radio': <Input type="radio" label="label" />,
+  'Input/radio/checked': <Input type="radio" label="label" checked />,
+  'Input/radio/success': <Input type="radio" label="label" hasSuccess />,
+  'Input/radio/error': <Input type="radio" label="label" hasError />,
+  'Input/textarea/default': <Input type="textarea" label="label" placeholder="placeholder" />,
+  'Input/textarea/secondary': <Input type="textarea" label="label" placeholder="placeholder" variant="secondary" />,
+  'Input/textarea/success': <Input type="textarea" label="label" placeholder="placeholder" hasSuccess />,
+  'Input/textarea/error': <Input type="textarea" label="label" placeholder="placeholder" hasError />,
+  'Input/textarea/warning': <Input type="textarea" label="label" placeholder="placeholder" hasWarning />,
+};
+
+export default symbols;
