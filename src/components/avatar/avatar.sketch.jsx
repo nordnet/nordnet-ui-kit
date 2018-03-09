e178639740115c52e16bad42a958195378098ee3
Mark Lenser <mlenser@gmail.com>
2018-03-09 10:01:58 +0100

    chore(avatar): setup avatar sketch

---
+++ src/components/avatar/avatar.sketch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Avatar from './avatar';
+
+const symbols = {
+  'Avatar/xs': <Avatar size="xs">xs</Avatar>,
+  'Avatar/sm': <Avatar size="sm">sm</Avatar>,
+  'Avatar/md': <Avatar size="md">md</Avatar>,
+  'Avatar/lg': <Avatar size="lg">lg</Avatar>,
+};
+
+export default symbols;
