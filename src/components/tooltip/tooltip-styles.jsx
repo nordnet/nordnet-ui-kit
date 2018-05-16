c37dd1c76d9442c062034abc44a1c89b93ee0372
Jacob Blomgren <jacob.blomgren@nordnet.se>
2018-05-16 18:15:15 +0200

    fix(tooltip): remove all dynamic stylesheets for tooltip
    Replaced the arrow function classes with two seperate ones for fixed width and normal width.

--- src/components/tooltip/tooltip-styles.jsx
+++ src/components/tooltip/tooltip-styles.jsx
@@ -3,6 +3,92 @@ const zIndexTooltip = 2;
 export default theme => {
   const { palette, transitions, mixins, typography } = theme;
 
+  const popupContentBase = {
+    width: '100%',
+    textAlign: 'left',
+    fontSize: 12,
+    padding: '4px 16px',
+    color: palette.background.default,
+    background: palette.text.default,
+    fontFamily: typography.primary.default,
+    borderRadius: 4,
+  };
+
+  const popupBase = {
+    transition: transitions.create(['opacity']),
+    position: 'absolute',
+    zIndex: zIndexTooltip,
+    '&:before': {
+      position: 'absolute',
+      left: '50%',
+      transform: 'translateX(-50%)',
+      display: 'block',
+      content: '""',
+      width: 0,
+      height: 0,
+      zIndex: zIndexTooltip,
+    },
+
+    '&$above': {
+      left: '50%',
+      bottom: '100%',
+      transform: 'translateX(-50%)',
+      marginBottom: 8,
+      '&:before': {
+        bottom: -8,
+        borderTop: `8px solid ${palette.text.default}`,
+        borderRight: '8px solid transparent',
+        borderLeft: '8px solid transparent',
+      },
+    },
+
+    '&$below': {
+      left: '50%',
+      transform: 'translateX(-50%)',
+      marginTop: 8,
+      '&:before': {
+        top: -8,
+        borderLeft: '8px solid transparent',
+        borderRight: '8px solid transparent',
+        borderBottom: `8px solid ${palette.text.default}`,
+      },
+    },
+
+    '&$left': {
+      right: 8,
+      top: '50%',
+      marginRight: '100%',
+      transform: 'translate(0, -50%)',
+      '&:before': {
+        left: 'inherit',
+        right: -8,
+        marginTop: -6,
+        top: '50%',
+        content: '""',
+        borderTop: '6px solid transparent',
+        borderBottom: '6px solid transparent',
+        borderLeft: `6px solid ${palette.text.default}`,
+      },
+    },
+
+    '&$right': {
+      left: 8,
+      top: '50%',
+      marginLeft: '100%',
+      transform: 'translate(0, -50%)',
+      '&:before': {
+        right: 'inherit',
+        left: -2,
+        marginTop: -6,
+        top: '50%',
+        content: '""',
+        borderTop: '6px solid transparent',
+        borderBottom: '6px solid transparent',
+        borderRight: `6px solid ${palette.text.default}`,
+      },
+    },
+  };
+
   return {
     tooltip: {
       ...mixins.basicBoxSizing,
@@ -53,97 +139,26 @@ export default theme => {
       },
     },
 
-    popup: {
-      transition: transitions.create(['opacity']),
-      position: 'absolute',
-      zIndex: zIndexTooltip,
-
-      width: ({ fixedWidth }) => fixedWidth || 'initial',
-      whiteSpace: ({ fixedWidth }) => (fixedWidth ? 'inherit' : 'nowrap'),
-
-      '&:before': {
-        position: 'absolute',
-        left: '50%',
-        transform: 'translateX(-50%)',
-        display: 'block',
-        content: '""',
-        width: 0,
-        height: 0,
-        zIndex: zIndexTooltip,
-      },
-
-      '&$above': {
-        left: '50%',
-        bottom: '100%',
-        transform: 'translateX(-50%)',
-        marginBottom: 8,
-        '&:before': {
-          bottom: -8,
-          borderTop: `8px solid ${palette.text.default}`,
-          borderRight: '8px solid transparent',
-          borderLeft: '8px solid transparent',
-        },
-      },
-
-      '&$below': {
-        left: '50%',
-        transform: 'translateX(-50%)',
-        marginTop: 8,
-        '&:before': {
-          top: -8,
-          borderLeft: '8px solid transparent',
-          borderRight: '8px solid transparent',
-          borderBottom: `8px solid ${palette.text.default}`,
-        },
-      },
-
-      '&$left': {
-        right: 8,
-        top: '50%',
-        marginRight: '100%',
-        transform: 'translate(0, -50%)',
-        '&:before': {
-          left: 'inherit',
-          right: -8,
-          marginTop: -6,
-          top: '50%',
-          content: '""',
-          borderTop: '6px solid transparent',
-          borderBottom: '6px solid transparent',
-          borderLeft: `6px solid ${palette.text.default}`,
-        },
-      },
-
-      '&$right': {
-        left: 8,
-        top: '50%',
-        marginLeft: '100%',
-        transform: 'translate(0, -50%)',
-        '&:before': {
-          right: 'inherit',
-          left: -2,
-          marginTop: -6,
-          top: '50%',
-          content: '""',
-          borderTop: '6px solid transparent',
-          borderBottom: '6px solid transparent',
-          borderRight: `6px solid ${palette.text.default}`,
-        },
-      },
+    popupNormal: {
+      ...popupBase,
+      whiteSpace: 'nowrap',
+    },
+    popupFixed: {
+      ...popupBase,
+      whiteSpace: 'inherit',
     },
 
-    popupContent: {
-      width: '100%',
-      textAlign: 'left',
-      fontSize: 12,
-      padding: '4px 16px',
-      color: palette.background.default,
-      background: palette.text.default,
-      fontFamily: typography.primary.default,
-      borderRadius: 4,
-      wordBreak: ({ fixedWidth }) => (fixedWidth ? 'break-word' : 'normal'),
-      overflowWrap: ({ fixedWidth }) => (fixedWidth ? 'break-word' : 'normal'),
-      wordWrap: ({ fixedWidth }) => (fixedWidth ? 'break-word' : 'normal'),
+    popupContentNormal: {
+      ...popupContentBase,
+      overflowWrap: 'normal',
+      wordBreak: 'normal',
+      wordWrap: 'normal',
+    },
+    popupContentFixed: {
+      ...popupContentBase,
+      overflowWrap: 'break-word',
+      wordBreak: 'break-word',
+      wordWrap: 'break-word',
     },
   };
 };
