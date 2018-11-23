dc7331a8d68f257f5731add4c25f109bb1a4296a
Fredrik Hallberg <fredrik.hallberg@nordnet.se>
2018-11-23 13:34:56 +0100

    feat: add tests for SmartTableContent

---
+++ test/components/smart-table-content/smart-table-content.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import { mockClasses } from '../../../src';
+import { Component as SmartTableContent, styles } from '../../../src/components/smart-table-content/smart-table-content';
+import TableHead from '../../../src/components/smart-table-content/TableHead/TableHead';
+import TableBody from '../../../src/components/smart-table-content/TableBody/TableBody';
+import Table from '../../../src/components/table';
+import Pagination from '../../../src/components/pagination';
+
+const classes = mockClasses(styles);
+
+const defaultProps = {
+  classes,
+  page: 1,
+  filters: { exchange_country: ['SE'], exchange_list: ['se:omxs30'] },
+  pageChangeHandler: () => {},
+  sortHandler: () => {},
+  sortField: 'name',
+  sortOrder: 'asc',
+  leftButtonsExperiment: false,
+  rowData: [0, 1, 2],
+  loading: false,
+  localization: {},
+};
+
+const shallowComponent = customProps => shallow(<SmartTableContent {...defaultProps} {...customProps} />);
+describe('<SmartTableContent />', () => {
+  it('has a Table', () => {
+    const component = shallowComponent();
+    const actual = component.find(Table).length;
+    const expected = 1;
+    expect(actual).to.equal(expected);
+  });
+
+  it('has a TableHead', () => {
+    const component = shallowComponent();
+    const actual = component.find(Table).find(TableHead).length;
+    const expected = 1;
+    expect(actual).to.equal(expected);
+  });
+
+  it('has a TableBody', () => {
+    const component = shallowComponent();
+    const actual = component.find(Table).find(TableBody).length;
+    const expected = 1;
+    expect(actual).to.equal(expected);
+  });
+
+  it('has pagination', () => {
+    const component = shallowComponent();
+    const actual = component.find(Pagination).length;
+    const expected = 1;
+    expect(actual).to.equal(expected);
+  });
+});
