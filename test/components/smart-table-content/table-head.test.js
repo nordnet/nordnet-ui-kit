a88c13a0685ea78fc6e5f5e2d30e22186ee88475
Fredrik Hallberg <fredrik.hallberg@nordnet.se>
2018-11-28 14:08:15 +0100

    fix: repair tests after refactoring

--- test/components/smart-table-content/table-head.test.js
+++ test/components/smart-table-content/table-head.test.js
@@ -7,7 +7,12 @@ import Tr from '../../../src/components/tr';
 import Th from '../../../src/components/th';
 import ArrowDownStraight from '../../../src/components/icon/icons/arrowDownStraight';
 import ArrowUpStraight from '../../../src/components/icon/icons/arrowUpStraight';
-import { Component as TableHead, styles } from '../../../src/components/smart-table-content/table-head/table-head';
+import {
+  Component as TableHead,
+  styles,
+  TableHeadContent,
+  TableHeadIcon,
+} from '../../../src/components/smart-table-content/table-head/table-head';
 
 const classes = mockClasses(styles);
 
@@ -132,69 +137,11 @@ describe('<TableHead />', () => {
     expect(actual).to.equal(true);
   });
 
-  it('Should render value in Button', () => {
+  it('Should render TableHeadContent in Button', () => {
     const component = shallowComponent({
       columns: [defaultColumn],
     });
-
-    const actual = component
-      .find('button')
-      .find(`span.${classes.columnHeaderInner}`)
-      .childAt(0)
-      .text();
-
-    const expected = defaultColumn.headerLabel;
-    expect(actual).to.equal(expected);
-  });
-
-  it('Should render value in Th', () => {
-    const component = shallowComponent({
-      columns: [
-        {
-          ...defaultColumn,
-          headerLabel: 'FOO_RANDOM',
-        },
-      ],
-    });
-    const actual = component
-      .find(Th)
-      .find('.columnHeaderInner')
-      .text();
-
-    const expected = 'FOO_RANDOM<ArrowDownStraight />';
-    expect(actual).to.equal(expected);
-  });
-
-  it('Should render arrow down', () => {
-    const component = shallowComponent({
-      columns: [defaultColumn],
-    });
-    const actual = component.find('button').find(ArrowDownStraight).length;
-    const expected = 1;
-    expect(actual).to.equal(expected);
-  });
-
-  it('Should render arrow up', () => {
-    const component = shallowComponent({
-      columns: [defaultColumn],
-      sortField: 'name',
-      sortOrder: 'asc',
-    });
-
-    const actual = component.find('button').find(ArrowUpStraight).length;
-
-    const expected = 1;
-    expect(actual).to.equal(expected);
-  });
-
-  it('Should render arrow down if not sorted in this column', () => {
-    const component = shallowComponent({
-      columns: [defaultColumn],
-      sortField: 'isin',
-      sortOrder: 'desc',
-    });
-
-    const actual = component.find('button').find(ArrowDownStraight).length;
+    const actual = component.find('button').find(TableHeadContent).length;
 
     const expected = 1;
     expect(actual).to.equal(expected);
@@ -210,30 +157,78 @@ describe('<TableHead />', () => {
     const expected = false;
     expect(actual).to.equal(expected);
   });
+});
+
+describe('<TableHeadIcon />', () => {
+  it('arrow should point up if sorted ascending', () => {
+    const component = shallow(<TableHeadIcon classes={classes} sortOrder={'asc'} isSortedOn />);
+    expect(component.find(ArrowUpStraight).length).to.equal(1);
+  });
+  it('arrow should point down if sorted descending', () => {
+    const component = shallow(<TableHeadIcon classes={classes} sortOrder={'desc'} isSortedOn />);
+    expect(component.find(ArrowDownStraight).length).to.equal(1);
+  });
+  it('arrow should point down if not sorted on', () => {
+    const component = shallow(<TableHeadIcon classes={classes} sortOrder={'asc'} isSortedOn={false} />);
+    expect(component.find(ArrowDownStraight).length).to.equal(1);
+  });
+});
+
+describe('<TableHeadContent />', () => {
+  const props = {
+    colSpec: {
+      headerLabel: 'TestLabel',
+      align: 'left',
+      sortable: true,
+    },
+    classes,
+    sortOrder: 'asc',
+    isSortedOn: true,
+  };
+  it('component should render label and icon', () => {
+    const component = shallow(<TableHeadContent {...props} />);
+    const actualLabel = component
+      .find(`span.${classes.columnHeaderInner}`)
+      .childAt(0)
+      .text();
+
+    const actualArrow = component
+      .find(`span.${classes.columnHeaderInner}`)
+      .childAt(1)
+      .text();
+
+    expect(actualLabel).to.equal('TestLabel');
+    expect(actualArrow).to.equal('<TableHeadIcon />');
+  });
 
   it('render class if the table is sorted on this column', () => {
-    const component = shallowComponent({
-      columns: [defaultColumn],
-      sortField: 'name',
-      sortOrder: 'asc',
-    });
-
+    const component = shallow(<TableHeadContent {...props} />);
     const actual = component.find('.columnHeaderSorted').length;
 
     const expected = 1;
     expect(actual).to.equal(expected);
   });
-
   it('dont render class if the table is sorted on another column', () => {
-    const component = shallowComponent({
-      columns: [defaultColumn],
-      sortField: 'isin',
-      sortOrder: 'asc',
-    });
+    const component = shallow(<TableHeadContent {...props} isSortedOn={false} />);
 
     const actual = component.find('.columnHeaderSorted').length;
 
     const expected = 0;
     expect(actual).to.equal(expected);
+  });
+  it('component should render icon to the left', () => {
+    const component = shallow(<TableHeadContent {...props} colSpec={{ ...props.colSpec, align: 'right' }} />);
+    const actualLabel = component
+      .find(`span.${classes.columnHeaderInner}`)
+      .childAt(1)
+      .text();
+
+    const actualArrow = component
+      .find(`span.${classes.columnHeaderInner}`)
+      .childAt(0)
+      .text();
+
+    expect(actualLabel).to.equal('TestLabel');
+    expect(actualArrow).to.equal('<TableHeadIcon />');
   });
 });
