ce9e66481c7b4ae2b3872800dc03e6b0021bd477
Mansour J. Mansour <mansour.mansour@nordnet.se>
2018-12-20 15:38:20 +0100

    fix: make caption visible if wanted

--- src/components/table-content-wrapper/table-content-wrapper.styles.js
+++ src/components/table-content-wrapper/table-content-wrapper.styles.js
@@ -1,4 +1,11 @@
-export default ({ mixins, palette }) => ({
+export default ({ mixins, palette, typography }) => ({
+  caption: {
+    fontSize: 12,
+    fontWeight: typography.fontWeightSemiBold,
+    textAlign: 'left',
+    padding: [10, 4],
+    borderBottom: [2, 'solid', palette.color.grayDarker],
+  },
   screenReadersOnly: mixins.screenReadersOnly,
   feedback: {
     fontStyle: 'italic',
