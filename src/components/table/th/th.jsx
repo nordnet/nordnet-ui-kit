ac710e0c5afc131e708a7bd7b09cfedbdac47919
Fredrik Hallberg <fredrik.hallberg@nordnet.se>
2019-01-16 14:09:12 +0100

    fix: fix border bottom rendering on sticky headers in edge

--- src/components/table/th/th.jsx
+++ src/components/table/th/th.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TableCell from '../table-cell';
 
 export default function Th(props) {
-  return <TableCell tagName="th" {...props} />;
+  const { style, ...rest } = props;
+
+  const thStyle = {
+    height: '100%',
+    ...style,
+  };
+
+  return <TableCell tagName="th" style={thStyle} {...rest} />;
 }
+
+Th.propTypes = {
+  style: PropTypes.object,
+};
