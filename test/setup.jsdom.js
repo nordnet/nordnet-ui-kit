c56dea7b388265667512b9a99cce663406e5a988
Jacob Blomgren <blomgren@protonmail.com>
2019-01-30 18:10:39 +0100

    chore: bump more dependencies

--- test/setup.jsdom.js
+++ test/setup.jsdom.js
@@ -1,11 +1,11 @@
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
-const jsdom = require('jsdom');
+const { JSDOM } = require('jsdom');
 
 Enzyme.configure({ adapter: new Adapter() });
 
 // prettier-ignore
-global.document = jsdom.jsdom('<html><head><script></script></head><body><div id="app"></div></body></html>');
+global.document = (new JSDOM('<html><head><script></script></head><body><div id="app"></div></body></html>')).window.document;
 global.window = document.defaultView;
 global.navigator = global.window.navigator;
 
