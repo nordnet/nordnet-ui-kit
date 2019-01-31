70f9cedf5f2bce01bdd527445fc5b2abfb9641b2
Fredrik Hallberg <saccer92@gmail.com>
2019-01-31 14:07:45 +0100

    feat(Rating): add Rating component (#515)
    * feat(Rating): add Rating component

---
+++ src/components/rating/rating.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import injectSheet from 'react-jss';
+import cn from 'classnames';
+import { Icon as UiKitIcon } from '../../';
+import styles from './rating.styles';
+
+/* eslint-disable react/no-array-index-key */
+const Rating = ({ icon: Icon = UiKitIcon.Star, size, rating, maxRating, classes }) => (
+  <div className={classes.rating}>
+    {[...Array(maxRating)].map((_, index) => {
+      const classNames = cn({
+        [classes.active]: index < rating,
+        [classes.inactive]: index >= rating,
+        [classes.ratingIcon]: true,
+      });
+      return (
+        <Icon
+          key={index}
+          className={classNames}
+          fill="currentColor"
+          stroke="currentColor"
+          width={size}
+          height={size}
+        />
+      );
+    })}
+  </div>
+);
+
+Rating.defaultProps = {
+  maxRating: 5,
+  size: 16,
+};
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+  maxRating: PropTypes.number,
+  icon: PropTypes.func,
+  size: PropTypes.number,
+  classes: PropTypes.shape().isRequired,
+};
+
+export { Rating as Component, styles };
+export default injectSheet(styles)(Rating);
