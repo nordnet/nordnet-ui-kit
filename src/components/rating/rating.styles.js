e2c548e05cd0895f192eb8e2d70777c7935a2f6e
Fredrik Hallberg <fredrik.hallberg@nordnet.se>
2019-01-31 13:37:23 +0100

    feat(Rating): add Rating component

---
+++ src/components/rating/rating.styles.js
@@ -0,0 +1,11 @@
+export default ({ palette }) => ({
+  ratingIcon: {
+    marginRight: 2,
+  },
+  active: {
+    color: palette.accent,
+  },
+  inactive: {
+    color: palette.background.secondary,
+  },
+});
