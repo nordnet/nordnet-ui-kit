1e2f455a029465595830888d47ce45ca2aba1e2c
Vladimir Starkov <iamstarkov@gmail.com>
2019-03-07 14:42:20 +0100

    wip

--- src/styles/color.js
+++ src/styles/color.js
@@ -1,15 +1,23 @@
 /*
 
+BRAND
+
+BrandBlue #00C8F5
+BrandGreen #D2F500
+BrandPink #FF2B83
+BrandTurquoise #00F0E1
+
+
 COMPLEMENTARY BRAND COLOURS
 
-ComplementaryTurquoise #01424C
-ComplementaryTurquoise #009195
-ComplementaryPink #78013A
-ComplementaryPink #AC135A
-ComplementaryGreen #023C00
-ComplementaryGreen #3A913F
-ComplementaryBlue #131F4F
 ComplementaryBlue1 #385E9D
+ComplementaryBlue2 #131F4F
+ComplementaryGreen1 #3A913F
+ComplementaryGreen2 #023C00
+ComplementaryPink1 #AC135A
+ComplementaryPink2 #78013A
+ComplementaryTurquoise1 #009195
+ComplementaryTurquoise2 #01424C
 
 
 GRAYSCALE PALETTE
@@ -23,7 +31,7 @@ Gray3 #A0A09B
 Gray2 #6E6E69
 Gray1 #4B4B46
 Gray0 #282823
-GrayBackground #F5F5F5
+Gray7 #F5F5F5
 
 
 FUNCTIONAL COLOURS FOR PRODUCT DEVELOPMENT
@@ -31,7 +39,7 @@ FUNCTIONAL COLOURS FOR PRODUCT DEVELOPMENT
 Index #FFCF00
 Negative #FF1900
 Positive #00D200
-CTAR0 #0046FF
+CTA #0046FF
 
 */
 
@@ -45,21 +53,33 @@ export default {
   // gray0
   grayDark: '#888790', // Gray text on white bg, icon
 
-  // 
+  // gray4 
   gray: '#B9B6C2', // Borders on input fields
+  // gray5 (if too light, go with gray4)
   grayLight: '#CFCFD5', // Dividers on gray bg
+  // gray6
   grayLighter: '#E4E4E8', // Dividers on white bg
+  // gray7
   grayLightest: '#F6F6F6', // Backgrounds
 
+  // cta
   blue: '#00A9EC', // Logo, links, cta
+  // complementaryBlue1
   blueDark: '#365299', // Info color
+  // complementaryBlue2
   blueDarker: '#2B417A', // Used together with dark in header gradient
+  // brandBlue // add comments to clarify with designers
   blueLight: '#E3F2FC', // Background color in alerts
+  // positive
   green: '#26A699', // Plus performance, success
+  // negative
   red: '#FE6F67', // Minus performance, error
+  // negative
   redLight: '#FE6F67', // Sell
-
+  
+  // yellow
   yellow: '#FFD75A', // Notification Logo
+  // yellow
   yellowLight: '#FFF1DD', // Notification
   white: '#FFF', // Background
 };
