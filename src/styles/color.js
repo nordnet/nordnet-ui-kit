c27db6d2b78b309e115c4f19de83a93bc56a2a8e
Vladimir Starkov <iamstarkov@gmail.com>
2019-03-07 13:34:10 +0100

    wip

--- src/styles/color.js
+++ src/styles/color.js
@@ -1,9 +1,51 @@
+/*
+
+COMPLEMENTARY BRAND COLOURS
+
+ComplementaryTurquoise #01424C
+ComplementaryTurquoise #009195
+ComplementaryPink #78013A
+ComplementaryPink #AC135A
+ComplementaryGreen #023C00
+ComplementaryGreen #3A913F
+ComplementaryBlue #131F4F
+ComplementaryBlue1 #385E9D
+
+
+GRAYSCALE PALETTE
+
+Black #000000
+White #FFFFFF
+Gray6 #EBEBE8
+Gray5 #D7D7D2
+Gray4 #BCBCB6
+Gray3 #A0A09B
+Gray2 #6E6E69
+Gray1 #4B4B46
+Gray0 #282823
+GrayBackground #F5F5F5
+
+
+FUNCTIONAL COLOURS FOR PRODUCT DEVELOPMENT
+
+Index #FFCF00
+Negative #FF1900
+Positive #00D200
+CTAR0 #0046FF
+
+*/
+
 export default {
+  // gray0 
   black: '#373640', // Black text on white
+  // gray0
   grayDarkest: '#4C4A56', // Avatar bg
+  // gray0
   grayDarker: '#6A6775', // Text on light gray bg
+  // gray0
   grayDark: '#888790', // Gray text on white bg, icon
 
+  // 
   gray: '#B9B6C2', // Borders on input fields
   grayLight: '#CFCFD5', // Dividers on gray bg
   grayLighter: '#E4E4E8', // Dividers on white bg
