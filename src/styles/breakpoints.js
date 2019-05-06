98f672091dc0cfa3087fe7075f14d3a4367ce85a
Jacob Blomgren <blomgren@protonmail.com>
2019-05-06 12:01:06 +0200

    feat: add new breakpoints

--- src/styles/breakpoints.js
+++ src/styles/breakpoints.js
@@ -1,9 +1,10 @@
 const breakpoints = {
   // The grid is mobile first so xs is the default, hence no breakpoint
   xs: false,
-  sm: 768,
-  md: 992,
-  lg: 1300,
+  sm: 720,
+  md: 936,
+  lg: 1252,
+  xl: 1600,
 };
 
 export default function createBreakpoints() {
